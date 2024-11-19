def151b5e9aaf0fd9021c9f5cd39e6e18a0485c9
hwk9764 <soccering17@gmail.com>
2024-11-19 12:01:01 +0900

    feature/pdf_complete

--- resources/pdf2data.py
+++ resources/pdf2data.py
@@ -2,7 +2,6 @@ import re
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 import pandas as pd
-# from tqdm import tqdm
 
 
 def extract_text_by_side(page_layout, side="left"):
@@ -15,11 +14,11 @@ def extract_text_by_side(page_layout, side="left"):
             text = element.get_text()
             if side == "left" and element.x0 < page_layout.width / 2:
                 # 왼쪽 페이지 상단 불필요 텍스트 제거
-                text = re.sub(r'2024년도\s+국가공무원\s+5급\s+공채\s+등\s+필기시험', '', text)
+                text = re.sub(r'2023년도\s+국가공무원\s+5급\s+공채\s+등\s+필기시험', '', text)
                 elements.append(re.sub(r"\n", "", text))
             elif side == "right" and element.x0 >= page_layout.width / 2:
                 # 오른쪽 페이지 상단 불필요 텍스트 제거
-                text = re.sub(r'언어논리영역|책형|[0-9]+쪽', '', text)
+                text = re.sub(r'언어논리영역|가 책형|[0-9]+쪽', '', text)
                 elements.append(re.sub(r"\n", "", text))
     return ''.join(elements)
 
@@ -51,20 +50,35 @@ def process_single_problem(problem_text):
         # print(question_match)
         if question_match:
             index,question=question_match.group().split(".")
-
             index=index.strip()
             question=question.strip()
             context_start=question_match.end()
+
+            
             context_choices_split=re.split(r"(?=\n?[①-⑤]\s)",problem_text[context_start:],maxsplit=1)
-            context = context_choices_split[0].strip()
+            
+            context_question_plus = context_choices_split[0].strip()
+
+            context_question_plus_split= re.split(r"<보    기>",context_question_plus,maxsplit=1)
+
+
+            if len(context_question_plus_split)>1:
+                context=context_question_plus_split[0].strip()
+                question_plus=context_question_plus_split[1].strip()
+            else:
+                context=context_question_plus_split[0].strip()
+                question_plus=None
+
             choices_text=context_choices_split[1]
             choices = re.split(r"(?=[①-⑤])", choices_text)
             choices = [re.sub(r"^[①-⑤]\s?", "", choice.strip()) for choice in choices if choice.strip()]
+            
             return{
-                "Index": int(index),
-                "Question": question,
-                "Context": context,
-                "Choices": choices
+                "id": int(index),
+                "question": question,
+                "paragraph": context,
+                "choices": choices,
+                "question_plus":question_plus
             }
     except Exception as e:
         print(f"[Error] 단일 문제를 처리하는 중 오류 발생: {e}")
@@ -104,15 +118,18 @@ def extract_page_data(file_path, side="left"):
     return pd.DataFrame(data)
 
 
-
-# 사용 예시
-file_path = "./aug_data/psat_언어논리_2024.pdf"
+file_path = "./aug_src/2023/언어논리(가).pdf"
 left_data = extract_page_data(file_path, side="left")
 right_data = extract_page_data(file_path, side="right")
 
-
-# 출력 예시
 total_data = pd.concat((left_data, right_data), axis=0)
-total_data = total_data.sort_values(by=['Index'])
-total_data = total_data.dropna(subset=['Context']).reset_index(drop=True)
-total_data.to_csv("extract_from_pdf.csv", encoding='utf-8-sig')
+total_data = total_data.sort_values(by=['id'])
+
+ans_df=pd.read_csv("./aug_src/2023/answer.csv")
+total_data = pd.merge(total_data, ans_df, on="id", how="right")
+
+total_data = total_data[total_data['paragraph'].str.strip() != ""]
+
+total_data = total_data.dropna(subset=['paragraph'])
+
+total_data.to_csv("./aug_src/2023/extract_from_pdf.csv", encoding='utf-8-sig')
