b0e82cba3ac25e20161e9e38c57fe4959e76b54a
suvinn <suvii6603@gmail.com>
2024-11-21 13:02:56 +0000

    processing data aug

--- data_aug/sat_history/generate_paragraph.py
+++ data_aug/sat_history/generate_paragraph.py
@@ -3,17 +3,14 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from tqdm import tqdm
 
-def load_model_locally(path="../../saved_model"):
-    """모델과 토크나이저를 메모리 효율적으로 로드"""
-    model = AutoModelForCausalLM.from_pretrained(
-        path,
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
-        device_map="auto",
-    )
-    model.gradient_checkpointing_enable()
-    tokenizer = AutoTokenizer.from_pretrained(path)
-    return model, tokenizer
+
+model = AutoModelForCausalLM.from_pretrained(
+    "LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct",
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained("LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct")
 
 def generate_paragraph(input_file, output_file, batch_size, model, tokenizer):
     df = pd.read_csv(input_file)
@@ -26,7 +23,7 @@ def generate_paragraph(input_file, output_file, batch_size, model, tokenizer):
 
             for _, row in batch.iterrows():
                 question = row['question']
-                choices = eval(row['choices'])  # 문자열을 리스트로 변환
+                choices = eval(row['choices'])
                 answer = row['answer']
 
                 prompt = f"""
@@ -55,7 +52,7 @@ def generate_paragraph(input_file, output_file, batch_size, model, tokenizer):
                     output = model.generate(
                         input_ids,
                         eos_token_id=tokenizer.eos_token_id,
-                        max_new_tokens=500,  # 지문 길이를 늘림
+                        max_new_tokens=500,
                     )
                     response = tokenizer.decode(output[0], skip_special_tokens=True)
                     generated_paragraph = response.split("[|assistant|]")[-1].strip()
@@ -72,7 +69,6 @@ def generate_paragraph(input_file, output_file, batch_size, model, tokenizer):
     print(f"\n결과가 {output_file}에 저장되었습니다.")
 
 if __name__ == "__main__":
-    input_file = "sat_us_history_processed.csv"
+    input_file = "sat_us_history_with_text_answer.csv"
     output_file = "sat_us_history_paragraph.csv"
-    model, tokenizer = load_model_locally()
     generate_paragraph(input_file=input_file, output_file=output_file, batch_size=1, model=model, tokenizer=tokenizer)
