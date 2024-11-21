f814436beba50b1e4e8ea0614a0eeddd5c83f3d1
sunghoon014 <sunghoon014@gmail.com>
2024-11-21 02:13:47 +0000

    update

--- model/bm25.py
+++ model/bm25.py
@@ -3,10 +3,11 @@ import pickle
 import logging
 import numpy as np
 from tqdm import tqdm
+import bm25s
 from rank_bm25 import BM25Okapi
 from kiwipiepy import Kiwi
 
-kiwi = Kiwi()
+kiwi = Kiwi(num_workers=8)
 
 LOGGER = logging.getLogger()
 
@@ -48,7 +49,7 @@ class BM25Reranker(object):
                 for x in morph_question_form:
                     morph_question_form_hf.extend(self.tokenizer.tokenize(x))
             except:
-                morph_question_form = kiwi.tokenize(txt)
+                morph_question_form_hf = self.tokenizer.tokenize(x)
             tokenized_text.append(morph_question_form_hf)
         return tokenized_text
 
@@ -81,3 +82,78 @@ class BM25Reranker(object):
             bm25_scores.append(bm25_score)
 
         return np.array(bm25_scores)
+
+    def get_bm25_rank_scores(self, questions, doc_ids, top_k=25):
+        tokenized_questions = self._tokenize(questions)
+        print(tokenized_questions)
+        bm25_score = self.model.get_batch_scores(tokenized_questions, doc_ids)
+        print(bm25_score)
+        exit()
+        top_k_indices = []
+        for qustion in tqdm(tokenized_questions):
+            bm25_score = self.model.get_scores(qustion)
+            indices = np.argsort(bm25_score)[-top_k:][::-1]
+            print(indices)
+            # scores = [bm25_score[i] for i in indices]
+            top_k_indices.extend(indices)
+
+        return top_k_indices
+
+
+class BM25SModel:
+    def __init__(self, tokenizer=None, bm25_dir=None):
+        self.model = None
+        self.tokenizer = tokenizer
+
+        if bm25_dir:
+            self._load_bm25_pickle(bm25_dir)
+
+    def _load_bm25_pickle(self, bm25_dir):
+        print(">>> Loading BM25 model.")
+        self.model = bm25s.BM25.load(bm25_dir)
+
+    def _tokenize(self, text):
+        print(">>> Tokenize to kiwi")
+        results = kiwi.tokenize(text)
+        print(">>> Ensemble sub-word tokenizer")
+        tokenized_text = []
+        for result in tqdm(results, total=len(text)):
+            morph_question_form = [
+                x.form for x in result if x.tag in ["NNG", "NNP", "NNB", "NR", "NP", "SN", "SH", "SL", "VV", "VA", "XR"]
+            ]
+            try:
+                morph_question_form_hf = []
+                for x in morph_question_form:
+                    morph_question_form_hf.extend(self.tokenizer.tokenize(x))
+            except:
+                print("error")
+                morph_question_form_hf = [x.form for x in result]
+            tokenized_text.append(morph_question_form_hf)
+        return tokenized_text
+
+    def _prepend_title_to_text(self, text, title):
+        text_with_title = []
+        for _text, _title in zip(text, title):
+            text_with_title.append(_title + ". " + _text)
+        return text_with_title
+
+    def build_bm25_model(self, text, title=None, path="./bm25_model"):
+        if title:
+            prepended_text = self._prepend_title_to_text(text, title)
+            tokenized_text = self._tokenize(prepended_text)
+        else:
+            tokenized_text = self._tokenize(text)
+
+        print(">>> Training BM25 model...")
+        self.model = bm25s.BM25(corpus=tokenized_text)
+        self.model.index(tokenized_text)
+
+        print(">>> Training done.")
+        self.model.save(path)
+
+    def get_bm25_rank_scores(self, questions, topk):
+        tokenized_questions = self._tokenize(questions)
+        print(tokenized_questions[0])
+        batch_results = self.model.retrieve(tokenized_questions, k=topk)
+        indices = batch_results.documents.flatten()
+        return indices
