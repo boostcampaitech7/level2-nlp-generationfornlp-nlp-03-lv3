408cf0dda59e818b8d02e4846f5e2c91499db49a
sunghoon014 <sungoon014@gmail.com>
2024-11-22 02:18:16 +0000

    update unsloth requirements

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Python 환경 설정
-source .venv_sh/bin/activate
+source .venv/bin/activate
 
 # 현재 디렉토리 출력
 echo "현재 작업 디렉토리: $(pwd)"
@@ -10,8 +10,8 @@ echo "현재 작업 디렉토리: $(pwd)"
 echo "train.py 실행 중..."
 python train_unsloth.py --model_name_or_path hungun/Qwen2.5-14B-Instruct-kowiki-qa
 python train_unsloth.py --model_name_or_path ludobico/gemma2_9b_it_1ep_kowiki
-python train_unsloth.py --model_name_or_path MLP-KTLim/llama-3-Korean-Bllossom-8B
 python train_unsloth.py --model_name_or_path /dev/shm/lcw99/llama-3-10b-wiki-240709-f
+# python train_unsloth.py --model_name_or_path MLP-KTLim/llama-3-Korean-Bllossom-8B
 
 # 실행 완료 메시지
 echo "train.py 실행 완료"
